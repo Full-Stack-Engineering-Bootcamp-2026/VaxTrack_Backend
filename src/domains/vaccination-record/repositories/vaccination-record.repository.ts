9e9f40e459f5157dc776327118611aaf4f0c88d8
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-05-07 16:43:16 +0530

    feat:add apis neccessary for dashboard

--- src/domains/vaccination-record/repositories/vaccination-record.repository.ts
+++ src/domains/vaccination-record/repositories/vaccination-record.repository.ts
@@ -35,4 +35,91 @@ export class VaccinationRecordRepository {
     async findOverDueVaccines(): Promise<VaccinationRecord[]> {
         return this.repository.find({ where: { dueDate: LessThan(new Date()), status: Not(status.COMPLETED) } });
     }
+
+    async countCompleted(guardianId?: number): Promise<number> {
+        if (guardianId) {  //guardian
+            return this.repository.count({
+                where: { status: status.COMPLETED, dependent: { guardian: { id: guardianId } } }, relations: {
+                    dependent: {
+                        guardian: true
+                    }
+                }
+            });
+        }
+        return this.repository.count({ where: { status: status.COMPLETED } }); //admin and staff
+    }
+
+    async countUpcoming(guardianId?: number): Promise<number> {
+        if (guardianId) {  //guardian
+            return this.repository.count({
+                where: { status: status.UPCOMING, dependent: { guardian: { id: guardianId } } }, relations: {
+                    dependent: {
+                        guardian: true
+                    }
+                }
+            });
+        }
+        return this.repository.count({ where: { status: status.UPCOMING } }); //admin and staff
+    }
+
+    async countOverdue(guardianId?: number): Promise<number> {
+        if (guardianId) {  //guardian
+            return this.repository.count({
+                where: { status: status.OVERDUE, dependent: { guardian: { id: guardianId } } }, relations: {
+                    dependent: {
+                        guardian: true
+                    }
+                }
+            });
+        }
+        return this.repository.count({ where: { status: status.OVERDUE } }); //admin and staff
+    }
+
+    async findUpcomingVaccines(guardianId?: number): Promise<VaccinationRecord[]> {
+        if (guardianId) {  //guardian
+            return this.repository.find({
+                where: { status: status.UPCOMING, dependent: { guardian: { id: guardianId } } }, relations: {
+                    vaccine: true,
+                    dependent: true
+                },
+                order: {
+                    dueDate: "ASC"
+                },
+                take: 10
+            });
+        }
+        return this.repository.find({
+            where: { status: status.UPCOMING }, relations: {
+                vaccine: true,
+                dependent: true
+            },
+            order: {
+                dueDate: "ASC"
+            },
+            take: 10
+        });
+    }
+
+    async findOverdueVaccines(guardianId?: number): Promise<VaccinationRecord[]> {
+        if (guardianId) {  //guardian
+            return this.repository.find({
+                where: { status: status.OVERDUE, dependent: { guardian: { id: guardianId } } }, relations: {
+                    vaccine: true,
+                    dependent: true
+                },
+                order: {
+                    dueDate: "ASC"
+                },
+            });
+        }
+        return this.repository.find({
+            where: { status: status.OVERDUE }, relations: {
+                vaccine: true,
+                dependent: true
+            },
+            order: {
+                dueDate: "ASC"
+            }
+        });
+    }
 }
